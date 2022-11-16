138f6628c3e1a61aea0ab5225c75b320ddcf7acd
116KBr <2768918334@qq.com>
2022-11-16 18:53:20 +0800

    Fix: debug findPrime

--- Task2_Encrypted_Coding/README.md
+++ Task2_Encrypted_Coding/README.md
@@ -1,1 +1,7 @@
 python version：3.9.12
+
+Ref:
+
+[密码学基础：AES加密算法 - 知乎 (zhihu.com)](https://zhuanlan.zhihu.com/p/78913397)
+
+[RSA周边——大素数是怎样生成的？ (bindog.github.io)](http://bindog.github.io/blog/2014/07/19/how-to-generate-big-primes/)
--- Task2_Encrypted_Coding/RSA/MontMod.py
+++
@@ -1,9 +0,0 @@
-def montMod(base, expo, divisor):
-#计算base^expo(mod divisor)
-    out = 1
-    binexpo = bin(int(expo))[::-1] 
-    temp = base #底数小于除数，即a mod n = a (a < n)
-    for ii in range(0, len(binexpo) - 2):
-        if(binexpo[ii] == '1'): out = (out * temp) % divisor
-        temp = (temp ** 2) % divisor
-    return out
